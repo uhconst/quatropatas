823d31bf525defad0556c87ebc815623e2e8c0e9
uhconst <uryelhenrique.c@gmail.com>
2017-02-06 12:53:42 +0100

    Iniciado o projeto com STS

---
+++ src/main/java/com/uhc/quatropatas/QuatropatasApplication.java
@@ -0,0 +1,12 @@
+package com.uhc.quatropatas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuatropatasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuatropatasApplication.class, args);
+	}
+}
