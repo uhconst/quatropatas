14c30d44e691a0dd9c619c3eb8d5e0836600e6f8
uhconst <uryelhenrique.c@gmail.com>
2017-02-08 17:06:58 +0100

    Implementado Login e tela 403 para acesso negado

---
+++ src/main/java/com/uhc/quatropatas/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.uhc.quatropatas.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.inMemoryAuthentication()
+			.withUser("joao").password("joao").roles("PESQUISAR_RACA").and()
+			.withUser("maria").password("maria").roles("CADASTRAR_RACA", "PESQUISAR_RACA");
+	}
+	
+	/*
+	 * Para ignorar o bloqueio ao layout na tela de login.
+	 * Assim ele dará acesso ao layout antes da pessoa logar.
+	 * Depois de feito o login, terá acesso ao resto.
+	 */
+	@Override
+	public void configure(WebSecurity web) throws Exception{
+		web.ignoring()
+			.antMatchers("/layout/**");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http
+			.authorizeRequests()
+				.antMatchers("/racas").hasRole("PESQUISAR_RACA")
+				.antMatchers("/racas/**").hasRole("CADASTRAR_RACA")
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/login")
+				.permitAll()
+				.and()
+			.logout()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+	}
+}
