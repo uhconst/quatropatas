6496836502d1475a12e0548094eebd2fc37e0754
uhconst <uryelhenrique.c@gmail.com>
2017-02-09 12:24:22 +0100

    Consertando erro de acesso. Deixando vermelho campos do form errados

--- src/main/java/com/uhc/quatropatas/config/SecurityConfig.java
+++ src/main/java/com/uhc/quatropatas/config/SecurityConfig.java
@@ -15,8 +15,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		auth.inMemoryAuthentication()
 			.withUser("joao").password("joao").roles("PESQUISAR_RACA").and()
 			.withUser("maria").password("maria").roles("CADASTRAR_RACA", "PESQUISAR_RACA").and()
-			.withUser("admin").password("").roles("CADASTRAR_RACA", "PESQUISAR_RACA")
-				.roles("CADASTRAR_ANIMAL", "PESQUISAR_ANIMAL");
+			.withUser("admin").password("").roles("CADASTRAR_RACA", "PESQUISAR_RACA", 
+					"CADASTRAR_ANIMAL", "PESQUISAR_ANIMAL");
 	}
 	
 	/*
