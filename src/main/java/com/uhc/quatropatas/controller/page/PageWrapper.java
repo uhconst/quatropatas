ea003c7a120ed984acc0cd358b874485ac01c503
uhconst <uryelhenrique.c@gmail.com>
2017-03-30 13:18:30 -0300

    Implementando a ordenação do Nome e Espécie na pesquisa de Raças

--- src/main/java/com/uhc/quatropatas/controller/page/PageWrapper.java
+++ src/main/java/com/uhc/quatropatas/controller/page/PageWrapper.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -67,5 +69,49 @@ public class PageWrapper<T> {
 	 */
 	public String urlParaPagina(int pagina) {
 		return uriBuilder.replaceQueryParam("page", pagina).build(true).encode().toUriString();
+	}
+	
+	public String urlOrdenada(String propriedade){
+		UriComponentsBuilder uriBuilderOrder = UriComponentsBuilder
+				.fromUriString(uriBuilder.build(true).encode().toUriString());
+		
+		String valorSort = String.format("%s,%s", propriedade, inverterDirecao(propriedade));
+				
+		return uriBuilderOrder.replaceQueryParam("sort", valorSort).build(true).encode().toUriString();
+	}
+	
+	/*
+	 * Invertendo a ordenação de um para outro
+	 */
+	public String inverterDirecao(String propriedade){
+		String direcao = "asc";
+
+		Order order = page.getSort() != null ? page.getSort().getOrderFor(propriedade) : null;
+		if(order != null){
+			direcao = Sort.Direction.ASC.equals(order.getDirection()) ? "desc" : "asc";
+		}
+
+		return direcao;
+	}
+	
+	/*
+	 * Comparando se é ascendente no momento ou não para mudar o Caret no HTML
+	 */
+	public boolean descendente(String propriedade){
+		return inverterDirecao(propriedade).equals("asc");
+	}
+	
+	/*
+	 * Comparando se está ordenado para remover o Caret no HTML caso
+	 * não esteja ordenado ainda
+	 */
+	public boolean ordenada(String propriedade){
+		Order order = page.getSort() != null ? page.getSort().getOrderFor(propriedade) : null;
+
+		if(order == null){
+			return false;
+		}
+
+		return page.getSort().getOrderFor(propriedade) != null ? true : false;
 	}
 }
--- src/main/java/com/uhc/quatropatas/repository/helper/raca/RacasImpl.java
+++ src/main/java/com/uhc/quatropatas/repository/helper/raca/RacasImpl.java
@@ -6,11 +6,13 @@ import javax.persistence.PersistenceContext;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.util.StringUtils;
 
 import com.uhc.quatropatas.model.Raca;
@@ -38,6 +40,13 @@ public class RacasImpl implements RacasQueries{
 		criteria.setFirstResult(primeiroRegistro);
 		criteria.setMaxResults(totalRegistrosPorPagina);
 		
+		Sort sort = pageable.getSort();
+		if(sort != null){
+			Sort.Order order = sort.iterator().next();
+			String property = order.getProperty();
+			criteria.addOrder(order.isAscending() ? Order.asc(property) : Order.desc(property));
+		}
+		
 		adicionarFiltro(filtro, criteria);
 		return new PageImpl<>(criteria.list(), pageable, total(filtro));
 	}
@@ -60,6 +69,5 @@ public class RacasImpl implements RacasQueries{
 			}
 		}
 	}
-
 
 }
