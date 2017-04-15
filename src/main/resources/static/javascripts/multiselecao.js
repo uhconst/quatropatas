779bcdae0b5b9cd78c797650c3a23317d2a8ceaf
uhconst <uryelhenrique.c@gmail.com>
2017-04-15 19:57:42 -0300

    Selecionar todos os checkbox implementado. Também desativando os botões ativar/desativar quando não tem nada selecionado

--- src/main/resources/static/javascripts/multiselecao.js
+++ src/main/resources/static/javascripts/multiselecao.js
@@ -5,10 +5,13 @@ Quatropatas.MultiSelecao = (function() {
 	function MultiSelecao() {
 		this.statusBtn = $('.js-status-btn');
 		this.selecaoCheckbox = $('.js-selecao');
+		this.selecaoTodosCheckbox = $('.js-selecao-todos');
 	}
 	
 	MultiSelecao.prototype.iniciar = function() {
 		this.statusBtn.on('click', onStatusBtnClicado.bind(this));
+		this.selecaoTodosCheckbox.on('click', onSelecaoTodosClicado.bind(this));
+		this.selecaoCheckbox.on('click', onSelecaoClicado.bind(this));
 	}
 	
 	function onStatusBtnClicado(event) {
@@ -37,6 +40,22 @@ Quatropatas.MultiSelecao = (function() {
 			});
 			
 		}
+	}
+	
+	function onSelecaoTodosClicado() {
+		var status = this.selecaoTodosCheckbox.prop('checked');
+		this.selecaoCheckbox.prop('checked', status);
+		statusBotaoAcao.call(this, status);
+	}
+	
+	function onSelecaoClicado() {
+		var selecaoCheckboxChecados = this.selecaoCheckbox.filter(':checked');
+		this.selecaoTodosCheckbox.prop('checked', selecaoCheckboxChecados.length >= this.selecaoCheckbox.length);
+		statusBotaoAcao.call(this, selecaoCheckboxChecados.length);
+	}
+	
+	function statusBotaoAcao(ativar) {
+		ativar ? this.statusBtn.removeClass('disabled') : this.statusBtn.addClass('disabled');
 	}
 	
 	return MultiSelecao;
